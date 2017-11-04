266f7939f945494897d782151103f62bc51d69b1
Dant86 <vedantdpathak@iCloud.com>
2017-11-04 14:36:48 -0500

    match get and post

--- Snakefile
+++ Snakefile
@@ -1,12 +1,15 @@
 from app.squash_main import db
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+DOTENV_PATH = join(dirname(__file__), '.env')
+load_dotenv(DOTENV_PATH)
+P_USER = os.environ.get("POSTGRES_USER")
+P_PASS = os.environ.get("POSTGRES_PASS")
+LINK = "postgresql://{}:{}@localhost/squash_db".format(P_USER, P_PASS)
 
 rule run:
 	shell:
 		"python -B app/squash_main.py"
-
-rule db_create:
-
-rule db_drop:
-	run:
-		db.drop()
-
--- app/squash_main.py
+++ app/squash_main.py
@@ -1,12 +1,14 @@
 from flask import *
+from flask.ext.session import Session
 import sqlite3
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, or_
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import datetime
+import bcrypt
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
 postgres_user = os.environ.get("POSTGRES_USER")
 postgres_pass = os.environ.get("POSTGRES_PASS")
@@ -15,6 +17,9 @@ conn = "postgresql://{}:{}@localhost/squash_db".format(postgres_user, postgres_p
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = conn
 db = SQLAlchemy(app)
+
+sess = Session()
+sess.init(app)
 
 class Admin(db.Model):
 	__tablename__ = "squash_admin"
@@ -29,7 +34,7 @@ class User(db.Model):
 	l_name = db.Column('last_name', db.Unicode)
 	u_name = db.Column('username', db.Unicode)
 	pass_w = db.Column('password', db.Unicode)
-	rank = db.Column('rank', db.Integer)
+	rank = db.Column('rank', db.Integer, primary_key=True)
 
 	def __init__(self, username, password):
 		self.u_name = username
@@ -57,9 +62,22 @@ class Match(db.Model):
 	score4 = db.Column('score4', db.Unicode)
 	score5 = db.Column('score5', db.Unicode)
 
+	def __init__(self, winner, loser, score1, score2, score3, score4, score5):
+		self.winner = winner
+		self.loser = loser
+		self.score1 = score1
+		self.score2 = score2
+		self.score3 = score3
+		self.score4 = score4
+		self.score5 = score5
+
 @app.route("/")
 def main_ting():
-	return redirect("/login")
+	return redirect("/home")
+
+@app.route("/home")
+def homepage():
+	return render_template("home.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def prompt_login():
@@ -68,6 +86,8 @@ def prompt_login():
 	if request.method == "POST":
 		uname = request.form.get("username")
 		passw = request.form.get("password")
+		user = User.query.filter(User.u_name.like())
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def show_signup_page():
@@ -76,15 +96,42 @@ def show_signup_page():
 	if request.method == "POST":
 		uname = request.form.get("username")
 		passw = request.form.get("password")
+		passw = bcrypt.hashpw(passw, bcrypt.gensalt())
 		new_user = User(uname, passw)
 		db.session.add(new_user)
 		db.session.commit()
+		sess["user"] = new_user
 		return redirect("/roster")
+
+@app.route("/matches", methods=["GET", "POST"])
+def matches():
+	if request.method == "GET":
+		all_matches = Match.query.all()
+		return render_template("matches.html", matches=all_matches)
+	if request.method == "POST":
+		winner = request.form.get("winner")
+		loser = request.form.get("loser")
+		game1 = request.form.get("game1")
+		game2 = request.form.get("game2")
+		game3 = request.form.get("game3")
+		game4 = request.form.get("game4")
+		game5 = request.form.get("game5")
+		new_match = Match(winner, loser, game1, game2, game3, game4, game5)
+		winner = User.query.filter(or_(User.f_name.like(winner[:winner.index(" ")]),
+			User.l_name.like(winner[winner.index(" ")+1:])))[0]
+		loser = User.query.filter(or_(User.f_name.like(loser[:loser.index(" ")]),
+			User.l_name.like(loser[loser.index(" ")+1:])))[0]
+		db.session.add(new_match)
+		db.session.commit()
+		return redirect("/matches")
 
 @app.route("/roster", methods=["GET"])
 def show_roster():
 	users = User.query.all()
+	users.sort(ley=lambda x: x.rank)
 	return render_template("roster.html", users=users)
+
+@app.route()
 
 if __name__ == "__main__":
 	app.run(debug=True)
